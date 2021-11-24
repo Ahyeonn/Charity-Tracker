a5d06001caeb3b7de1f004586ff5a2d1156594ff
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-11-24 02:24:01 -0800

    Users can see single donation history

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+import datetime
 
 client = MongoClient()
 db = client.CharityTracker
@@ -7,7 +9,31 @@ charities = db.charities
 
 app = Flask(__name__)
 
+@app.route('/charitylists')
+def charity_index():
+    """Show all charitylists"""
+    return render_template('donation.html')
+
 @app.route('/feeddonate')
-def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', charities=charities.find())
+def feed_index_new():
+    """show & create dbs for the form"""
+    return render_template('feeddonate_index&new.html', charities=charities.find())
+#skip feed new because it will be on the same page
+#form for new & for loops for index
+
+@app.route('/feeddonate', methods=['POST'])
+def feed_submit():
+    """Submit the form of a donation for the feed charity"""
+    feed = {
+        'feed_name': request.form.get('feed_name'),
+        'feed_donation': request.form.get('feed_donation'),
+        'feed_date': request.form.get('feed_date')
+    }
+    charities.insert_one(feed)
+    return redirect(url_for('feed_show', feed_id=feed['_id']))
+
+@app.route('/charities/<feed_id>')
+def feed_show(feed_id):
+    """Show a single playlist."""
+    feed = charities.find_one({'_id': ObjectId(feed_id)})
+    return render_template('feeddonate_show.html', feed=feed)
